b7c71667549f4a079005effc8aebb541be6d0eb9
Alex Heredia <alexheredia@Alexs-MacBook-Air.local>
2026-05-20 07:57:31 -0600

    Change logo

--- src/main.js
+++ src/main.js
@@ -242,7 +242,7 @@ function renderLoginScreen() {
   overlay.id = 'login-overlay';
   overlay.innerHTML = `
     <div class="login-card">
-      <div class="login-logo"><img src="public/img/logo.png"></div>
+      <div class="login-logo"><img src="/img/logo.png" alt="Logo BUAP"></div>
       <h1 class="login-title">BUAP Campus</h1>
       <p class="login-sub">Inicia sesión para continuar</p>
 
